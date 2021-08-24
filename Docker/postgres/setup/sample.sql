82c48a072771c33839d605c554edc3cc48a465f0
石井将司 <optacos@gmail.com>
2021-08-24 20:41:37 +0900

    UPDATE: POST CRUD の実装(API)

---
+++ Docker/postgres/setup/sample.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts(content, author) VALUES ('Hello world!', 'Sau sheong');
+INSERT INTO posts(content, author) VALUES ('¡Hola mond!', 'Adam');
+INSERT INTO posts(content, author) VALUES ('Guten tag!', 'Betty');
+INSERT INTO posts(content, author) VALUES ('Bon jour!', 'Cathrine');
+INSERT INTO posts(content, author) VALUES ('Zdravst vuyte!', 'Daria');
